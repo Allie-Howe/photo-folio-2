b0511bf8b928fb45bc16bfa506ea3627f26db168
Allie Howe <howeisallie@gmail.com>
2023-10-30 00:25:31 +0000

    added birds to grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import { useSpring, animated } from '@react-spring/web'
 import { useDrag } from '@use-gesture/react'
-import { range } from 'lodash';
+import { birds } from './birds';
 
-function PullRelease() {
+function PullRelease({bird}: {bird: string}) {
   const [{ x, y }, api] = useSpring(() => ({ x: 0, y: 0 }))
 
   // Set the drag hook and define component movement based on gesture data
   const bind = useDrag(({ last, movement: [mx, my] }) => {
     const down = !last;
-    api.start({ x: down ? mx : 0, y: down ? my : 0, immediate: false, config: {
+    api.start({ x: down ? mx : 0, y: down ? my : 0, immediate: down, config: {
       mass: 1,
       friction: 12,
       tension: 500,
@@ -16,14 +16,16 @@ function PullRelease() {
   })
 
   // Bind it to a component
-  return <animated.div {...bind()} style={{ x, y }} className='h-10 w-10 bg-white' />
+  return <animated.div {...bind()} style={{ x, y }} className={'touch-none w-full h-full'}>
+    <div className='rounded-2xl w-full aspect-[3/2] bg-contain bg-center scale-100' style={{ backgroundImage: `url('${bird}')` }} />
+  </animated.div>
 }
 
 function App() {
   return (
     <div className='h-[100dvh] overflow-hidden bg-black text-white'>
-      <div className='grid grid-cols-3 w-full h-full overflow-auto gap-10 place-items-center p-10'>
-        {range(150).map(i => <PullRelease key={i} />)}
+      <div className='grid md:grid-cols-3 w-full h-full overflow-auto gap-10 place-items-center p-10'>
+        {birds.map(bird => <PullRelease key={bird} bird={bird} />)}
       </div>
     </div>
   )
---
+++ src/birds.ts
@@ -0,0 +1,28 @@
+import bird1 from './assets/birds/IMG 0042.jpg'
+import bird2 from './assets/birds/IMG 0134.jpg'
+import bird3 from './assets/birds/IMG 0182.jpg'
+import bird4 from './assets/birds/IMG 0267.jpg'
+import bird5 from './assets/birds/IMG 3669.jpg'
+import bird6 from './assets/birds/IMG 4033.jpg'
+import bird7 from './assets/birds/IMG 4556.jpg'
+import bird8 from './assets/birds/IMG 4845.jpg'
+import bird10 from './assets/birds/IMG 7522.jpg'
+import bird11 from './assets/birds/IMG 8214.jpg'
+import bird12 from './assets/birds/IMG 8293.jpg'
+import bird14 from './assets/birds/IMG 9315.jpg'
+export const birds = [
+  bird1,
+  bird2,
+  bird3,
+  bird4,
+  bird5,
+  bird6,
+  bird7,
+  bird8,
+  // bird9,
+  bird10,
+  bird11,
+  bird12,
+  // bird13,
+  bird14
+]
