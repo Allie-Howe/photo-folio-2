3f8b0c9deec76cf270f75484f8d04b2e7f095859
Allie Howe <howeisallie@gmail.com>
2023-10-31 15:55:55 +0000

    DraggableImage its own file

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,7 @@
-import { useSpring, animated } from '@react-spring/web'
-import { useDrag } from '@use-gesture/react'
-import { FileResolutions, birds } from './birds';
+import { birds } from './birds';
 import { range, values } from 'lodash';
 import { useState } from 'react';
-
-function DraggableImage({file}: {file: FileResolutions}) {
-  const [{ x, y }, api] = useSpring(() => ({ x: 0, y: 0 }))
-
-  // Set the drag hook and define component movement based on gesture data
-  const bind = useDrag(({ last, movement: [mx, my] }) => {
-    const down = !last;
-    api.start({ x: down ? mx : 0, y: down ? my : 0, config: {
-      mass: 1,
-      friction: 12,
-      tension: 500,
-    } })
-  })
-
-  // Bind it to a component
-  return <animated.div {...bind()} style={{ x, y }} className={'touch-none w-full h-full'}>
-    <div className='rounded-2xl w-full aspect-[3/2] bg-contain bg-center' style={{ backgroundImage: `url('${file.low}')` }} />
-  </animated.div>
-}
+import { DraggableImage } from './DraggableImage';
 
 const BirdGrid = () => {
   return (
