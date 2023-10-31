af0796c37bfa321160f6117de0e976c7b4779c51
Allie Howe <howeisallie@gmail.com>
2023-10-31 19:50:22 +0000

    added dialog for full-size images

--- src/DraggableImage.tsx
+++ src/DraggableImage.tsx
@@ -1,8 +1,20 @@
 import { useSpring, animated } from '@react-spring/web';
 import { FileResolutions } from './files';
 import { useDrag } from '@use-gesture/react';
+import { UseState } from './ImageGrid';
+import { useCallback } from 'react';
 
-export function DraggableImage({file}: {file: FileResolutions}) {
+const ASPECT_RATIO = 'aspect-[3/2]'
+
+interface DraggableImageProps {
+  selectedImageState: UseState<string | undefined>
+  file: FileResolutions
+}
+export function DraggableImage({file, selectedImageState}: DraggableImageProps) {
+  const [selectedImage, setSelectedImage] = selectedImageState;
+
+  const resetSelected = useCallback(() => setSelectedImage(undefined), [setSelectedImage]);
+
   const [{ x, y }, api] = useSpring(() => ({ x: 0, y: 0 }))
 
   // Set the drag hook and define component movement based on gesture data
@@ -16,7 +28,22 @@ export function DraggableImage({file}: {file: FileResolutions}) {
   })
 
   // Bind it to a component
-  return <animated.div {...bind()} style={{ x, y }} className={'touch-none w-full h-full'}>
-    <div className='rounded-md w-full aspect-[3/2] bg-[length:100%] bg-center' style={{ backgroundImage: `url('${file.low}')`}} />
-  </animated.div>
+  return <>
+    <animated.div {...bind()} style={{ x, y }} className={'touch-none w-full h-full'}>
+      <div
+        className={`rounded-md w-full ${ASPECT_RATIO} bg-[length:100%] bg-center`}
+        style={{ backgroundImage: `url('${file.low}')`}}
+        onClick={() => setSelectedImage(file.low)}
+      />
+    </animated.div>
+    {/* TODO: This takes a long time to open - try to make it async and show a loading icon */}
+    <dialog open={selectedImage === file.low} className='select-none top-0 w-full h-[100dvh] bg-black text-white bg-opacity-70'>
+      <div className='flex flex-col w-full justify-center items-center h-full' onClick={resetSelected}>
+        <div className='relative'>
+          <img src={file.high} className={`max-h-[90dvh] max-w-[90dvw] ${ASPECT_RATIO}`} />
+          <p className='md:hidden absolute top-0 text-center w-full text-xl opacity-50' style={{textShadow: '0 0 5px black'}}>pinch to zoom</p>
+        </div>
+      </div>
+    </dialog>
+  </>
 }
--- src/ImageGrid.tsx
+++ src/ImageGrid.tsx
@@ -1,10 +1,15 @@
+import { useState } from 'react';
 import { DraggableImage } from './DraggableImage';
 import { FileResolutions } from './files';
 
+export type UseState<T> =  [T, React.Dispatch<React.SetStateAction<T>>]
+
 export const ImageGrid = ({files}: {files: FileResolutions[]}) => {
+  const selectedImageState = useState<string>();
+
 return (
   <div className='grid md:grid-cols-3 w-full h-full overflow-auto gap-10 p-10'>
-    {files.map(bird => <DraggableImage key={bird.low} file={bird} />)}
+    {files.map(bird => <DraggableImage key={bird.low} file={bird} selectedImageState={selectedImageState} />)}
   </div>
 );
 }
