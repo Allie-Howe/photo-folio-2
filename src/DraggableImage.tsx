792f86699712793d3b21c3306597aa8fb24f48a0
Allie Howe <howeisallie@gmail.com>
2023-11-01 22:16:01 +0000

    FullSizeImage its own component

--- src/DraggableImage.tsx
+++ src/DraggableImage.tsx
@@ -21,12 +21,20 @@ export function DraggableImage({file, selectedImageState}: DraggableImageProps) 
     />
     {/* TODO: This takes a long time to open - try to make it async and show a loading icon */}
     <dialog open={selectedImage === file.low} className='fixed select-none top-0 w-full h-[100dvh] bg-black text-white bg-opacity-70'>
-      <div className='flex flex-col w-full justify-center items-center h-full' onClick={resetSelected}>
-        <div className='relative'>
-          <img src={file.high} className={`max-h-[90dvh] max-w-[90dvw] ${ASPECT_RATIO}`} />
-          <p className='md:hidden absolute top-0 text-center w-full text-xl opacity-50' style={{textShadow: '0 0 5px black'}}>pinch to zoom</p>
-        </div>
-      </div>
+      <FullSizeImage resetSelected={resetSelected} file={file} />
     </dialog>
   </>
+}
+
+interface FullSizeImageProps {
+  resetSelected: () => void,
+  file: FileResolutions
+}
+function FullSizeImage({file, resetSelected}: FullSizeImageProps) {
+  return <div className='flex flex-col w-full justify-center items-center h-full' onClick={resetSelected}>
+    <div className='relative'>
+      <img src={file.high} className={`max-h-[90dvh] max-w-[90dvw] ${ASPECT_RATIO}`} />
+      <p className='md:hidden absolute top-0 text-center w-full text-xl opacity-50' style={{ textShadow: '0 0 5px black' }}>pinch to zoom</p>
+    </div>
+  </div>;
 }
