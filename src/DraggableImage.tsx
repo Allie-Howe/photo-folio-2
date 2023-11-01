b84b9f20a14aca60e4142dd8895ba61df6c93a49
Allie Howe <howeisallie@gmail.com>
2023-11-01 21:20:15 +0000

    DraggableImage no longer draggable

--- src/DraggableImage.tsx
+++ src/DraggableImage.tsx
@@ -1,6 +1,4 @@
-import { useSpring, animated } from '@react-spring/web';
 import { FileResolutions } from './files';
-import { useDrag } from '@use-gesture/react';
 import { UseState } from './ImageGrid';
 import { useCallback } from 'react';
 
@@ -15,27 +13,12 @@ export function DraggableImage({file, selectedImageState}: DraggableImageProps) 
 
   const resetSelected = useCallback(() => setSelectedImage(undefined), [setSelectedImage]);
 
-  const [{ x, y }, api] = useSpring(() => ({ x: 0, y: 0 }))
-
-  // Set the drag hook and define component movement based on gesture data
-  const bind = useDrag(({ last, movement: [mx, my] }) => {
-    const down = !last;
-    api.start({ x: down ? mx : 0, y: down ? my : 0, config: {
-      mass: 1,
-      friction: 12,
-      tension: 500,
-    } })
-  })
-
-  // Bind it to a component
   return <>
-    <animated.div {...bind()} style={{ x, y }} className={'touch-none w-full h-full'}>
-      <div
-        className={`rounded-md w-full ${ASPECT_RATIO} bg-[length:100%] bg-center`}
-        style={{ backgroundImage: `url('${file.low}')`}}
-        onClick={() => setSelectedImage(file.low)}
-      />
-    </animated.div>
+    <div
+      className={`rounded-md w-full ${ASPECT_RATIO} bg-[length:100%] bg-center`}
+      style={{ backgroundImage: `url('${file.low}')`}}
+      onClick={() => setSelectedImage(file.low)}
+    />
     {/* TODO: This takes a long time to open - try to make it async and show a loading icon */}
     <dialog open={selectedImage === file.low} className='fixed select-none top-0 w-full h-[100dvh] bg-black text-white bg-opacity-70'>
       <div className='flex flex-col w-full justify-center items-center h-full' onClick={resetSelected}>
